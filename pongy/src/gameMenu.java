a55d13bb30909cad3bdfd468c5ba0637f5638e29
trekk <meyjulien67@gmail.com>
2014-02-06 11:56:42 +0100

    basicgamestate

---
+++ pongy/src/gameMenu.java
@@ -0,0 +1,41 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class gameMenu extends BasicGameState
+{
+
+	private static int _ID = 1;
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame sbg, Graphics g) throws SlickException
+	{
+		
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getID()
+	{
+		return _ID;
+	}
+
+
+
+}
---
+++ pongy/src/optionsReader.java
@@ -0,0 +1,15 @@
+
+public class optionsReader
+{
+
+	static public String[] get()
+	{
+		//TODO fonction de read du xml
+		return null;
+	}
+	static public void set()
+	{
+		//TODO fonction de set du xml
+	}
+	
+}
